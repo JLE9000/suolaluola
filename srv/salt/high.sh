b83a627549c61f910a382d0a8aeb82515fd8fb82
Jere <jere.leppanen@myy.haaga-helia.fi>
2018-04-24 12:03:48 +0000

    Removed ''

--- srv/salt/high.sh
+++ srv/salt/high.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get -y install salt-minion
-echo 'master: 167.99.197.184\nid: jere' |sudo tee /etc/salt/minion
+echo master: 167.99.197.184\ id: jere |sudo tee /etc/salt/minion
 sudo salt-call --local state.highstate --file-root srv/salt
