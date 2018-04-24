3efeaddbe79e473d3f62d325be86995a947cb550
Jere <jere.leppanen@myy.haaga-helia.fi>
2018-04-24 11:56:56 +0000

    Installs salt-minion & highstate

--- srv/salt/high.sh
+++ srv/salt/high.sh
@@ -1,1 +1,4 @@
+sudo apt-get update
+sudo apt-get -y install salt-minion
+echo 'master: 167.99.197.184\nid: jere' |sudo tee /etc/salt/minion
 sudo salt-call --local state.highstate --file-root srv/salt
